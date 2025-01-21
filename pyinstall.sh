779b4a708f142af94f1f34b612a89f3ff2586bcd
Haakon Eidem Haakstad <157736192+HaakonEidemHaakstad@users.noreply.github.com>
2025-01-21 21:37:20 +0100

    Update pyinstall.sh

--- pyinstall.sh
+++ pyinstall.sh
@@ -6,6 +6,6 @@ sudo apt-get install build-essential gdb lcov pkg-config libbz2-dev libffi-dev l
 wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz
 tar -xvzf Python-3.13.0.tgz
 cd Python-3.13.0
-./configure --enable-optimizations --enable-shared
+./configure --enable-optimizations
 make -j4
 sudo make altinstall
